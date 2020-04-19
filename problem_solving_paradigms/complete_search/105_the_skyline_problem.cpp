f93bd4e7ff36c759ef85d70f74bcf1ec4feb64ac
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2020-04-18 17:55:27 -0700

    Solving the skyline problem (105)
    Also add a thinking file and a desktop test.

--- problem_solving_paradigms/complete_search/105_the_skyline_problem.cpp
+++ problem_solving_paradigms/complete_search/105_the_skyline_problem.cpp
@@ -9,6 +9,9 @@ struct Building
   int left; // Left or starting point of current in horizontal axis.
   int height; // Height of the current (vertical axis).
   int right; // Right or ending point of current in horizontal axis.
+
+  Building(int left = -1, int height = -1, int right = -1) : left(left), height(height), right(right)
+  { }
 };
 
 int main()
@@ -33,45 +36,91 @@ int main()
 
   // Compute the skyline of the buildings.
   vector<int> skyline;
-  Building current, next;
-  for(int i = 0, j = i + 1; i < buildings.length(); i++)
+  int size = buildings.length();
+  for(int i = 0, j = 1; i < size; i++)
   {
     // Current building.
-    current = buildings[i];
+    Building current = buildings[i];
 
-    // Draw the skyline.
-    skyline.push_back(buildings[i].left);
-    skyline.push_back(buildings[i].height);
+    // Draw the first coordinates in the skyline.
+    skyline.push_back(current.left);
+    skyline.push_back(current.height);
 
-    // Select buildings to pass above (discarding the hidden buildings).
-    vector<Building> insideRange;
-    for(; next.left < current.r; j++)
+    // Do the following until reach a separated building or an overlaping one.
+    Building previous, next;
+    Building xBuilding = Building(), yBuilding = Building(),overlapingBuilding = Building(); // It's the higher building with the greater range. // It's the building a step before in range and heigth of the closeleBuilding.
+    for(j = i + 1; j < size; j++)
     {
-      next = buildings[j]
-      bool visibleWithCurrentBuilding =
-        next.right > current.right || next.height > current.height;
+      previous = buildings[j-1];
+      next = buildings[j];
 
-      if(insideRange.length() == 0)
+      // TODO: make the adjustement for possible yBuilding incorrect analisis.
+      bool buildingsAreSeparated = next.left > xBuilding.right;
+      if(buildingsAreSeparated)
+        break;
+
+      // Delete hidden elements from the skyline.
+      //bool overlapsPreviousBuilding = (
+        //next.right > xBuilding.right &&
+        //next.height > yBuilding.height &&
+        //next.height > current.height &&
+        //next.right > current.right);
+
+      // TODO: Review the following analisis.
+      // Know when the following building hides the previous ones.
+      bool overlapsPreviousBuildings = (
+        next.left <= current.right &&
+        next.height >= yBuilding.height &&
+        next.right >= xBuilding.right);
+
+      if (overlapsPreviousBuildings)
+        skyline.erase(skyline.begin() + (i), skyline.end());
+
+      // Register in the skyline vector the corresponding coordinates.
+      bool exceedsPreviousRange = next.right > previous.right;
+      bool exceedsPreviousHeight = next.height > previous.height;
+
+      if(exceedsPreviousRange && exceedsPreviousHeight)
       {
-        if(visibleWithCurrentBuilding)
-          insideRange.push_back(next);
+        skyline.push_back(next.left);
+        stkyline.push_back(next.height);
+
+        // TODO: it's probably necessary to do something here.
       }
-      else
+      else if(exceedsPreviousRange && !exceedsPreviousHeight)
       {
-        bool visibleWithPreviousBuilding =
-        (
-          next.right > insideRange[j - 1].right &&
-          next.height > insideRange[j - 1].height
-        );
+        // Indicates x Coordinate
+        skyline.push_back(previous.right);
 
-        if(visibleWithCurrentBuilding || visibleWithPreviousBuilding)
-          insideRange.push_back(next);
+        // Indicates y coordinate.
+        bool existsTraversalBuilding = xBuilding.right > previous.right;
+        if(!existsTraversalBuilding)
+          skyline.push_back(next.height); // TODO: analize the previous line.
         else
-          insideRange.erase(insideRange.begin() + j - 1);
+          skyline.push_back(xBuilding.height);
       }
+      else if(!exceedsPreviousRange && exceedsPreviousHeight)
+      {
+        skyline.push_back(next.left);
+        skyline.push_back(next.height);
+
+        // TODO: it's probably necessary to do something here.
+      }
+
+      // TODO: There is a thing that it's necessary to be analized. :3
+      bool isHigherThanYBuilding = yBuilding.height < next.height;
+      bool exceedsXBuildingRange = xBuilding.right < next.right;
+
+      if(isHigherThanYBuilding)
+        yBuilding = next;
+      if(exceedsXBuildingRange)
+        xBuilding = next;
     }
 
-    // Avoid the processed buildings.
+    // Draw end building coordinates of the last one before an empty space.
+    skyline.push_back(previous.right);
+    skyline.push_back(0);
+
     i = j;
   }
 
